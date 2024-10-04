e59180393f17c2dc514efceeb1143f189547c92e
itsWindows11 <itswin11@outlook.com>
2024-10-04 19:32:29 +0300

    Added sitemap & last updated

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -82,7 +82,11 @@ export default defineConfig({
   title: "ThreadSharp",
   description: "A C# API wrapper for the Threads API.",
   cleanUrls: true,
+  lastUpdated: true,
   base: "/ThreadSharp/",
+  sitemap: {
+    hostname: "https://itswindows11.github.io/ThreadSharp"
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
